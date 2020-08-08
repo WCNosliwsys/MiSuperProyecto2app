360bd93d6f005aee438b0797aa5c661bad511eae
Wilson Callisaya <demarktec@gmail.com>
2020-08-07 21:15:53 -0500

    Usando Snackbar

--- app/src/main/java/com/ditec/misuperparte2/MainActivity.java
+++ app/src/main/java/com/ditec/misuperparte2/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     @Override
@@ -134,5 +135,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         else
             Toast.makeText(this, "Se pulso el floating button de la derecha "+ view.getTag(), Toast.LENGTH_LONG).show();
 
+    }
+
+    public void fabizquierda(View view) {
+        Snackbar.make(view, "Replace with your own action",
+                Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+    Snackbar mibar;
+    public void fabderecha(View view) {
+        mibar=Snackbar.make(view,"¿Estás seguro?", Snackbar.LENGTH_INDEFINITE)
+                .setAction("SI", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+//tu evento
+                        mibar.dismiss();
+                    }
+                });
+        mibar.show();
     }
 }
